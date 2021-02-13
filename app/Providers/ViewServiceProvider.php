14fadc6863b770d4edea6b8dcf5299a5cc7bf47e
mamun417 <git-002@devxhub.com>
2021-02-13 23:51:11 +0600

    logo added with email template

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -31,23 +31,26 @@ class ViewServiceProvider extends ServiceProvider
     public function boot()
     {
         // social icon all data get here
-        View::composer(['includes.header'], function ($view){
+        View::composer(['includes.header'], function ($view) {
             $globalSocialInfo = Social::status()->get();
             $view->with('globalSocialInfo', $globalSocialInfo);
         });
 
         // setting all data get here
         View::composer(
-            ['includes.header', 'admin.auth.login', 'admin.auth.reset', 'admin.auth.email'
-                , 'admin.auth.register'],
-            function ($view){
-            $globalSettingInfo = Setting::first();
-            $bg_image = SliderBg::first();
-            $view->with(compact('globalSettingInfo', 'bg_image'));
-        });
+            [
+                'includes.header', 'admin.auth.login', 'admin.auth.reset',
+                'admin.auth.email', 'admin.auth.register', 'admin.pages.messages.reply_message',
+                'pages.messages.message', 'emails.talent-message'
+            ],
+            function ($view) {
+                $globalSettingInfo = Setting::first();
+                $bg_image = SliderBg::first();
+                $view->with(compact('globalSettingInfo', 'bg_image'));
+            });
 
         // Contact Information all data get here
-        View::composer(['includes.footer', 'includes.contact-modal'], function ($view){
+        View::composer(['includes.footer', 'includes.contact-modal'], function ($view) {
             $globalContactInfo = Contact::first();
             $view->with('globalContactInfo', $globalContactInfo);
         });
--- resources/views/admin/pages/messages/reply_message.blade.php
+++ resources/views/admin/pages/messages/reply_message.blade.php
@@ -4,6 +4,11 @@
     <title>Reply</title>
 </head>
 <body>
+<div style="text-align: center">
+    <a href="{{ route('home') }}">
+        <img width="40%" src="{{ @$globalSettingInfo->image->url }}" alt="">
+    </a>
+</div>
 <h2><strong>Name: </strong>{{ $reply_details['name'] }}</h2>
 <hr>
 <p><strong>Email: </strong>{{ $reply_details['reply_email'] }}</p>
