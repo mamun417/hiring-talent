f24389c46029dae3d89b78894e5cec3951f0bd98
aziz417 <git-004@devxhub.com>
2021-02-14 20:38:47 +0600

    set link dashboard all cards

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,7 +6,8 @@
     <div class="">
         <div class="row wrapper border-bottom white-bg page-heading">
             <div class="col-lg-3">
-                <div class="widget style1 navy-bg">
+                <a href="#userList">
+                    <div class="widget style1 navy-bg">
                     <div class="row">
                         <div class="col-4">
                             <i class="fa fa-users fa-5x"></i>
@@ -17,49 +18,56 @@
                         </div>
                     </div>
                 </div>
+                </a>
             </div>
             <div class="col-lg-3">
-                <div class="widget style1 lazur-bg">
-                    <div class="row">
-                        <div class="col-4">
-                            <i class="fa fa-envelope-o fa-5x"></i>
-                        </div>
-                        <div class="col-8 text-right">
-                            <span> Total messages </span>
-                            <h2 class="font-bold">{{ @$messages->count() }}</h2>
+                <a href="{{ route('admin.messages.index') }}">
+                    <div class="widget style1 lazur-bg">
+                        <div class="row">
+                            <div class="col-4">
+                                <i class="fa fa-envelope-o fa-5x"></i>
+                            </div>
+                            <div class="col-8 text-right">
+                                <span> Total messages </span>
+                                <h2 class="font-bold">{{ @$messages->count() }}</h2>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </a>
             </div>
             <div class="col-lg-3">
-                <div class="widget style1 yellow-bg">
-                    <div class="row">
-                        <div class="col-4">
-                            <i class="fa fa-deaf fa-5x"></i>
-                        </div>
-                        <div class="col-8 text-right">
-                            <span> Total Talents </span>
-                            <h2 class="font-bold">{{ @$talents->count() }}</h2>
+                <a href="{{ route('admin.talents.index') }}">
+                    <div class="widget style1 yellow-bg">
+                        <div class="row">
+                            <div class="col-4">
+                                <i class="fa fa-deaf fa-5x"></i>
+                            </div>
+                            <div class="col-8 text-right">
+                                <span> Total Talents </span>
+                                <h2 class="font-bold">{{ @$talents->count() }}</h2>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </a>
             </div>
             <div class="col-lg-3">
-                <div class="widget style1 navy-bg">
-                    <div class="row">
-                        <div class="col-4">
-                            <i class="fa fa-image fa-5x"></i>
-                        </div>
-                        <div class="col-8 text-right">
-                            <span> Total Sliders </span>
-                            <h2 class="font-bold">{{ @$sliders->count() }}</h2>
+                <a href="{{ route('admin.sliders.index') }}">
+                    <div class="widget style1 navy-bg">
+                        <div class="row">
+                            <div class="col-4">
+                                <i class="fa fa-image fa-5x"></i>
+                            </div>
+                            <div class="col-8 text-right">
+                                <span> Total Sliders </span>
+                                <h2 class="font-bold">{{ @$sliders->count() }}</h2>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </a>
             </div>
 
             <div class="col-lg-12">
-                <div class="table-responsive m-t-md">
+                <div class="table-responsive m-t-md" id="userList">
                     <table class="table table-bordered table-hover">
                         <thead>
                         <tr>
